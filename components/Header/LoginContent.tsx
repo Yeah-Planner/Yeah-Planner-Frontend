8f8a9e3a27470b6465a34768ccd7b478e87a4b58
YEAHx4 <developerstarlight@gmail.com>
2021-11-21 10:17:23 +0900

    login interaction

--- components/Header/LoginContent.tsx
+++ components/Header/LoginContent.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
-import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { emailRegexp, pwRegexp } from '../../util/util'
 import {
   CreateAccountStyle,
   AuthBodyStyle,
@@ -13,6 +14,7 @@ import {
   AuthWithYeahContainerStyle,
   NotHaveAccountStyle,
   ToggleModeLinkStyle,
+  ErrorMessageStyle,
 } from './Login.style'
 
 interface Props {
@@ -20,18 +22,60 @@ interface Props {
 }
 
 const LoginContent: NextPage<Props> = ({ setIsLogin }) => {
+  const [badInput, setBadInput] = useState(false)
+  const [error, setError] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const checkInput = () => {
+    if (!emailRegexp.test(email)) {
+      setBadInput(true)
+      setError('유효하지 않은 이메일입니다.')
+    } else if (!pwRegexp.test(password)) {
+      setBadInput(true)
+      setError('비밀번호는 특수문자, 알파벳으로 8자리 이상이어야합니다.')
+    } else {
+      setBadInput(false)
+      setError('')
+    }
+  }
+
   return (
     <AuthContentStyle>
       <AuthBodyStyle>
         <AuthTitleStyle>로그인</AuthTitleStyle>
         <AuthWithTitleStyle>Yeah-Planner 계정으로 로그인</AuthWithTitleStyle>
-        <AuthWithYeahContainerStyle>
-          <AuthInputContainerStyle>
-            <AuthInputStyle type="email" placeholder="이메일" />
-            <AuthInputStyle type="password" placeholder="비밀번호" />
-          </AuthInputContainerStyle>
-          <AuthButtonStyle>로그인</AuthButtonStyle>
-        </AuthWithYeahContainerStyle>
+        <div>
+          <AuthWithYeahContainerStyle>
+            <AuthInputContainerStyle>
+              <AuthInputStyle
+                type="email"
+                placeholder="이메일"
+                value={email}
+                onChange={({ target: { value } }) => setEmail(value)}
+              />
+              <AuthInputStyle
+                type="password"
+                placeholder="비밀번호"
+                value={password}
+                onChange={({ target: { value } }) => setPassword(value)}
+              />
+            </AuthInputContainerStyle>
+            <AuthButtonStyle
+              onClick={e => {
+                e.preventDefault()
+                e.stopPropagation()
+                checkInput()
+                if (!badInput) {
+                  // TODO: Login Request
+                }
+              }}
+            >
+              로그인
+            </AuthButtonStyle>
+          </AuthWithYeahContainerStyle>
+          {badInput && <ErrorMessageStyle>{error}</ErrorMessageStyle>}
+        </div>
         <AuthWithTitleStyle>소셜 계정으로 로그인</AuthWithTitleStyle>
         <AuthWithSocialContainerStyle>Coming Soon</AuthWithSocialContainerStyle>
       </AuthBodyStyle>
