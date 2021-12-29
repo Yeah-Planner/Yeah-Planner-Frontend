d1a7e085a84728ace6f9d505d8ed2f28800e9da5
YEAHx4 <developerstarlight@gmail.com>
2021-12-29 23:08:39 +0900

    redirect to about page if not logged in

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const About: NextPage<{}> = () => {
+  return (
+    <>
+      <h1>About yeah-planner</h1>
+    </>
+  )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { getUser } from '../storage/storage'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    if (!getUser()) {
+      // not logged in
+      router.push('/about')
+    }
+  }, [])
+
   return <div>Hello World</div>
 }
 
