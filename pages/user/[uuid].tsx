2dd780a15a96b9585835373b80aab4dbb8fab36a
YEAHx4 <developerstarlight@gmail.com>
2022-01-21 08:28:15 +0900

    ssr profile page

--- pages/user/[uuid].tsx
+++ pages/user/[uuid].tsx
@@ -1,9 +1,9 @@
 import axios, { AxiosError } from 'axios'
-import { NextPage } from 'next'
-import { useEffect, useState } from 'react'
+import { GetServerSideProps, NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import ProfileContent from '../../components/Profile/ProfileContent'
 import { backend } from '../../util/util'
-import { useRouter } from 'next/router'
 
 export interface UserRes {
   uuid: string
@@ -14,37 +14,74 @@ export interface UserRes {
   updatedAt: Date
 }
 
-const ProfilePage: NextPage = () => {
+const ProfilePage: NextPage<{ data: UserRes }> = ({ data: user }) => {
+  // const router = useRouter()
+  // const { uuid } = router.query
+  // const [user, setUser] = useState<UserRes>()
+
+  // useEffect(() => {
+  //   console.log(uuid)
+  //   if (!uuid) return
+  //   ;(async () => {
+  //     try {
+  //       const { data, status } = await axios.get<UserRes>(
+  //         `${backend()}/user/?uuid=${uuid}`
+  //       )
+
+  //       if (status === 404) {
+  //         router.push('/404')
+  //       }
+
+  //       setUser(data)
+  //     } catch (error) {
+  //       if (axios.isAxiosError(error)) {
+  //         const err = error as AxiosError
+  //         if (err.response?.status === 404) {
+  //           // router.push('/404')
+  //         }
+  //       }
+  //     }
+  //   })()
+  // }, [uuid])
+
   const router = useRouter()
-  const { uuid } = router.query
-  const [user, setUser] = useState<UserRes>()
-
   useEffect(() => {
-    console.log(uuid)
-    if (!uuid) return
-    ;(async () => {
-      try {
-        const { data, status } = await axios.get<UserRes>(
-          `${backend()}/user/?uuid=${uuid}`
-        )
+    if (!user) router.push('/404')
+  }, [user])
+  return user && <ProfileContent data={user} />
+}
 
-        if (status === 404) {
-          router.push('/404')
-        }
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const { uuid } = ctx.query
+  if (!uuid) return { props: { data: null } }
+  try {
+    const { data, status } = await axios.get<UserRes>(
+      `${backend()}/user/?uuid=${uuid}`
+    )
 
-        setUser(data)
-      } catch (error) {
-        if (axios.isAxiosError(error)) {
-          const err = error as AxiosError
-          if (err.response?.status === 404) {
-            // router.push('/404')
-          }
-        }
+    if (status === 404) {
+      ctx.res.statusCode = 404
+    }
+
+    return {
+      props: {
+        data,
+      },
+    }
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const error = err as AxiosError
+      if (error.response?.status === 404) {
+        ctx.res.statusCode = 404
       }
-    })()
-  }, [uuid])
+    }
 
-  return user ? <ProfileContent data={user} /> : <>loading...</>
+    return {
+      props: {
+        data: null,
+      },
+    }
+  }
 }
 
 export default ProfilePage
