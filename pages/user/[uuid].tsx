1ccfe195de52a59a3d550864231df5be5b977050
YEAHx4 <developerstarlight@gmail.com>
2022-01-20 09:44:35 +0900

    fetch user profile data

--- pages/user/[uuid].tsx
+++ pages/user/[uuid].tsx
@@ -1,12 +1,44 @@
+import axios from 'axios'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { backend } from '../../util/util'
 
-interface Props {}
+export interface UserRes {
+  uuid: string
+  email: string
+  username: string
+  bio: string
+  createdAt: Date
+  updatedAt: Date
+}
 
-const ProfilePage: NextPage<Props> = ({}) => {
-  const { uuid } = useRouter().query
+const ProfilePage: NextPage = () => {
+  const router = useRouter()
+  const { uuid } = router.query
+  const [user, setUser] = useState<UserRes>()
 
-  return <>{uuid}</>
+  useEffect(() => {
+    ;(async () => {
+      const { data, status } = await axios.get<UserRes>(
+        `${backend()}/user/?uuid=${uuid}`
+      )
+
+      if (status === 404) {
+        router.push('/404')
+      }
+
+      setUser(data)
+    })()
+  }, [uuid])
+
+  return user ? (
+    <>
+      <h1>{user.username}</h1>
+    </>
+  ) : (
+    <>loading...</>
+  )
 }
 
 export default ProfilePage
