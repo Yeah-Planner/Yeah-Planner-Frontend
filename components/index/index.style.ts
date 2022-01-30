25bea5b1b3c1ecdf96dc3c81895ff6a97f3ab402
YEAHx4 <developerstarlight@gmail.com>
2022-01-31 01:28:03 +0900

    main title

---
+++ components/index/index.style.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const IndexContainerStyle = styled.div`
+  text-align: center !important;
+  margin-top: 17rem;
+
+  @media (max-width: 768px) {
+    margin-top: 5rem;
+  }
+`
+
+export const MainSloganStyle = styled.h1`
+  font-size: 4rem;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`
+
+export const MainSubSloganStyle = styled.h2`
+  margin-top: -2rem;
+  font-size: 1.8rem;
+  font-weight: normal;
+
+  @media (max-width: 768px) {
+    font-size: 1.3rem;
+    margin: 0;
+  }
+`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from 'next'
+import {
+  IndexContainerStyle,
+  MainSloganStyle,
+  MainSubSloganStyle,
+} from '../components/index/index.style'
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>
+  return (
+    <IndexContainerStyle>
+      <MainSloganStyle>
+        Manage everything you need to manage in one place with ease.
+      </MainSloganStyle>
+      <MainSubSloganStyle>
+        With Yeah Planner you can manage everything in one place. Yeah Planner
+        only requires Internet connection. What is you want to manage and
+        control? Yeah Planner will give a solution which is fit to what you
+        wanted to. Now let's create Yeah Planner account and start managing your
+        life.
+      </MainSubSloganStyle>
+    </IndexContainerStyle>
+  )
 }
 
 export default Home
