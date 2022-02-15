00168a7b2302fad58d84b0de193de0d703cee5e6
YEAHx4 <developerstarlight@gmail.com>
2022-02-16 00:16:48 +0900

    todo description

--- components/index/index.style.ts
+++ components/index/index.style.ts
@@ -27,3 +27,42 @@ export const MainSubSloganStyle = styled.h2`
     margin: 0;
   }
 `
+
+export const TodoContainerStyle = styled.div`
+  margin-top: 40vh;
+  width: 100%;
+  text-align: left;
+`
+
+export const TodoDescTitleStyle = styled.h2`
+  font-size: 2.2rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`
+
+export const TodoDescContainerStyle = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`
+
+export const TodoDescItemStyle = styled.div`
+  width: 100%;
+  padding: 1rem;
+`
+
+export const TodoImageStyle = styled.img`
+  width: 100%;
+  height: auto;
+`
+
+export const TodoItemTitleStyle = styled.h3`
+  font-size: 1.8rem;
+`
+
+export const TodoArticleStyle = styled.p`
+  font-size: 1.4rem;
+`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,33 @@
 import type { NextPage } from 'next'
-import {
-  IndexContainerStyle,
-  MainSloganStyle,
-  MainSubSloganStyle,
-} from '../components/index/index.style'
+import Home from '../components/index'
+// import {
+//   IndexContainerStyle,
+//   MainSloganStyle,
+//   MainSubSloganStyle,
+//   TodoContainerStyle,
+//   TodoDescTitleStyle,
+// } from '../components/index/index.style'
 
-const Home: NextPage = () => {
-  return (
-    <IndexContainerStyle>
-      <MainSloganStyle>
-        Manage everything you need to manage in one place with ease.
-      </MainSloganStyle>
-      <MainSubSloganStyle>
-        With Yeah Planner you can manage everything in one place. Yeah Planner
-        only requires Internet connection. What is you want to manage and
-        control? Yeah Planner will give a solution which is fit to what you
-        wanted to. Now let's create Yeah Planner account and start managing your
-        life.
-      </MainSubSloganStyle>
-    </IndexContainerStyle>
-  )
+const Index: NextPage = () => {
+  // return (
+  //   <IndexContainerStyle>
+  //     <MainSloganStyle>
+  //       Manage everything you need to manage in one place with ease.
+  //     </MainSloganStyle>
+  //     <MainSubSloganStyle>
+  //       With Yeah Planner you can manage everything in one place. Yeah Planner
+  //       only requires Internet connection. What is you want to manage and
+  //       control? Yeah Planner will give a solution which is fit to what you
+  //       wanted to. Now let's create Yeah Planner account and start managing your
+  //       life.
+  //     </MainSubSloganStyle>
+  //     <TodoContainerStyle>
+  //       <TodoDescTitleStyle>Feature: To-do</TodoDescTitleStyle>
+  //     </TodoContainerStyle>
+  //   </IndexContainerStyle>
+  // )
+
+  return <Home />
 }
 
-export default Home
+export default Index
